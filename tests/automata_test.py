a56322c61b56dc840ae0179bc8af391cd67c32f8
aiwaverse <aiwavision@protonmail.com>
2021-04-09 16:41:00 -0300

    Added check_word and break_word tests

--- tests/automata_test.py
+++ tests/automata_test.py
@@ -36,3 +36,38 @@ class TestAutomata:
         assert (
             TestAutomata.program_function == TestAutomata.aut.program_function
         )
+
+    def test_break_word(self):
+        word = "aaabbbbaaa"
+        assert TestAutomata.aut.break_word(word) == list(word)
+
+    def test_check_word_accept(self):
+        assert TestAutomata.aut.check_word("baaaa")[0]
+        assert TestAutomata.aut.check_word("baabbababaaa")[0]
+        assert TestAutomata.aut.check_word("a")[0]
+        assert TestAutomata.aut.check_word("baabaaa")[0]
+        assert TestAutomata.aut.check_word("bbaaaaaaaaabaa")[0]
+
+    def test_check_word_reject(self):
+        assert not TestAutomata.aut.check_word("aabbb")[0]
+        assert not TestAutomata.aut.check_word("abab")[0]
+        assert not TestAutomata.aut.check_word("aab")[0]
+        assert not TestAutomata.aut.check_word("bab")[0]
+        assert not TestAutomata.aut.check_word("")[0]
+
+    def test_check_word_accept_path(self):
+        word = "baaaa"
+        path = ["q0", "b", "q2", "a", "q3", "a", "q3", "a", "q3", "a", "q3"]
+        assert TestAutomata.aut.check_word(word)[1] == path
+
+    def test_check_word_reject_non_final(self):
+        assert (
+            TestAutomata.aut.check_word("ab")[1]
+            == "Program ended on non-final state q2."
+        )
+
+    def test_check_word_reject_undefined(self):
+        assert (
+            TestAutomata.aut.check_word("aaa")[1]
+            == "Program ended with undefined state at state q1 with element a."
+        )
