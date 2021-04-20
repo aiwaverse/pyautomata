e2d1551facdf93aab96c087bfb00685bc018f336
aiwaverse <aiwavision@protonmail.com>
2021-04-20 09:30:29 -0300

    Rewrote the test to use a setup method

--- tests/automata_parser_test.py
+++ tests/automata_parser_test.py
@@ -9,24 +9,24 @@ class TestAutomataParser:
     AutomataParser test class
     Run with python3.9 -m pytest
     """
-
-    test_string = "AUTÔMATO=({q0,q1,q2,q3},{a,b},Prog,q0,{q1,q3})\n\
-                   Prog\n\
-                   (q0,a)=q1\n\
-                   (q0,b)=q2\n\
-                   (q1,b)=q2\n\
-                   (q2,a)=q3\n\
-                   (q2,b)=q2\n\
-                   (q3,a)=q3\n\
-                   (q3,b)=q2"
-    p = AutomataParser(test_string)
-    description_dict, function_dict = p.parse()
+    def setup_method(self):
+        test_string = "AUTÔMATO=({q0,q1,q2,q3},{a,b},Prog,q0,{q1,q3})\n\
+                    Prog\n\
+                    (q0,a)=q1\n\
+                    (q0,b)=q2\n\
+                    (q1,b)=q2\n\
+                    (q2,a)=q3\n\
+                    (q2,b)=q2\n\
+                    (q3,a)=q3\n\
+                    (q3,b)=q2"
+        self.p = AutomataParser(test_string)
+        self.description_dict, self.function_dict = self.p.parse()
 
     def test_name(self):
-        assert TestAutomataParser.description_dict["name"] == "AUTÔMATO"
+        assert self.description_dict["name"] == "AUTÔMATO"
 
     def test_states(self):
-        assert TestAutomataParser.description_dict["states"] == [
+        assert self.description_dict["states"] == [
             "q0",
             "q1",
             "q2",
@@ -34,22 +34,22 @@ class TestAutomataParser:
         ]
 
     def test_alphabet(self):
-        assert TestAutomataParser.description_dict["alphabet"] == ["a", "b"]
+        assert self.description_dict["alphabet"] == ["a", "b"]
 
     def test_initial_state(self):
-        assert TestAutomataParser.description_dict["initial_state"] == "q0"
+        assert self.description_dict["initial_state"] == "q0"
 
     def test_final_states(self):
-        assert TestAutomataParser.description_dict["final_states"], [
+        assert self.description_dict["final_states"], [
             "q1",
             "q3",
         ]
 
     def test_transitions(self):
-        assert TestAutomataParser.function_dict[("q0", "a")] == "q1"
-        assert TestAutomataParser.function_dict[("q0", "b")] == "q2"
-        assert TestAutomataParser.function_dict[("q1", "b")] == "q2"
-        assert TestAutomataParser.function_dict[("q2", "a")] == "q3"
-        assert TestAutomataParser.function_dict[("q2", "b")] == "q2"
-        assert TestAutomataParser.function_dict[("q3", "a")] == "q3"
-        assert TestAutomataParser.function_dict[("q3", "b")] == "q2"
+        assert self.function_dict[("q0", "a")] == "q1"
+        assert self.function_dict[("q0", "b")] == "q2"
+        assert self.function_dict[("q1", "b")] == "q2"
+        assert self.function_dict[("q2", "a")] == "q3"
+        assert self.function_dict[("q2", "b")] == "q2"
+        assert self.function_dict[("q3", "a")] == "q3"
+        assert self.function_dict[("q3", "b")] == "q2"
