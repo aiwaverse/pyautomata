3487b7638c831b6d21b1c3d36880d18fb9aa692f
aiwaverse <aiwavision@protonmail.com>
2021-04-28 16:55:46 -0300

    Wrote remove unreachable states

---
+++ pyautomata/core/minimization.py
@@ -0,0 +1,35 @@
+from pyautomata.core.automata import Automata  # pylint: disable=import-error
+from typing import Dict, Set, Tuple, List
+
+
+class MinimizedAutomata(Automata):
+    def __init__(
+        self, program_function: Dict[Tuple[str, str], str], **kwargs
+    ) -> None:
+        super().__init__(program_function, **kwargs)
+
+    def remove_unreachable_states(self) -> None:
+        states = self.unreacheable_states()
+        for state in states:
+            for c in self.alphabet:
+                if (state, c) in self.program_function:
+                    self.program_function.pop((state, c))
+
+    def unreacheable_states(self) -> List[str]:
+        reacheable_states: Set[str] = set([self.initial_state])
+        new_states: Set[str] = set([self.initial_state])
+        while new_states:
+            temp: Set[str] = set()
+            for q in new_states:
+                for c in self.alphabet:
+                    p = self.program_function.get((q, c), "")
+                    if not p:
+                        continue
+                    temp = temp | set([p])
+            new_states = temp - reacheable_states
+            reacheable_states = reacheable_states | new_states
+        unreachable_states = set(self.states) - reacheable_states
+        return list(unreachable_states)
+
+    def minimize(self):
+        pass
