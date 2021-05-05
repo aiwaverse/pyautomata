ef86cd9e92a8c65a01c29cea19ce6bc81da118e9
aiwaverse <aiwavision@protonmail.com>
2021-05-05 14:47:06 -0300

    Re-wrote main to use the GUI class

--- main.py
+++ main.py
@@ -5,28 +5,24 @@ after installing the dependencies
 """
 import PySimpleGUI as sg
 
-from pyautomata.gui.automata_gui import (create_automata, read_word_file,
-                                         setup, test_word, test_word_pairs)
+from pyautomata import AutomataGUI
 
 
 def main():
     """
     The main function, basically handles everything
     """
-    aut_window = setup()
-    automata = None
+    gui = AutomataGUI()
     while True:
-        event, values = aut_window.read()
+        event, values = gui.read()
         if event in (sg.WIN_CLOSED, "Close"):
             break
         if event == "-AUTOMATA-SUBMIT-":
-            automata = create_automata(aut_window, values["-AUTOMATA-FILE-"])
-        elif event == "-WORD-FILE-SUBMIT-" and automata:
-            word_pairs = read_word_file(values["-WORD-FILE-"])
-            test_word_pairs(automata, word_pairs)
-        elif event == "-WORD-BUT-" and automata:
-            test_word(aut_window, values["-WORD-INPUT-"], automata)
-    aut_window.close()
+            gui.create_automata(values["-AUTOMATA-FILE-"])
+        elif event == "-WORD-FILE-SUBMIT-" and gui.automata:
+            gui.test_word_pairs(values["-WORD-FILE-"])
+        elif event == "-WORD-BUT-" and gui.automata:
+            gui.test_word(values["-WORD-INPUT-"])
 
 
 if __name__ == "__main__":
