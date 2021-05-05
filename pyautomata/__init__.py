00c95ef6db150a6dc0334e206a0a73a2f09f38f3
aiwaverse <aiwavision@protonmail.com>
2021-05-05 14:41:36 -0300

    added the GUI

--- pyautomata/__init__.py
+++ pyautomata/__init__.py
@@ -1,3 +1,4 @@
 from pyautomata.core.parser import AutomataParser, WordFileParser
 from pyautomata.core.automata import Automata
 from pyautomata.core.minimization import MinimizedAutomata
+from pyautomata.gui.automata_gui import AutomataGUI
