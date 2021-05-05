fe40bbb0d306bbcb5366e02f9990d497470aeb0d
aiwaverse <aiwavision@protonmail.com>
2021-05-05 14:41:27 -0300

    Wrote automata_gui

--- pyautomata/gui/automata_gui.py
+++ pyautomata/gui/automata_gui.py
@@ -1,157 +1,180 @@
 """
 The GUI functions for the program.
 """
-from typing import List, Tuple, Union
+from typing import List, Optional, Tuple, Union
 
 import PySimpleGUI as sg
 from more_itertools import grouper
 import pyautomata  # pylint: disable=import-error
 
 
-def create_automata(window: sg.Window, file: str) -> pyautomata.Automata:
+class AutomataGUI:
     """
-    Creates the automata via the Parser
-    Updates the text and buttons on screen
+    The GUI class for the program
     """
-    p = pyautomata.AutomataParser(file_name=file)
-    description, function_program = p.parse()
-    window["-AUTOMATA-LOADED-"].update("Loaded")
-    window["-WORD-BUT-"].update(disabled=False)
-    window["-WORD-FILE-SUBMIT-"].update(disabled=False)
-    aut = pyautomata.MinimizedAutomata(function_program, **description)
-    aut.minimize()
-    return aut
 
+    def __init__(self) -> None:
+        sg.theme("DarkGrey8")
+        layout = [
+            [
+                sg.Text("Automata: "),
+                sg.Text("Not loaded", key="-AUTOMATA-LOADED-"),
+            ],
+            [
+                sg.Text("Last word:"),
+                sg.Text("No word loaded", key="-WORD-TO-TEST-"),
+            ],
+            [
+                sg.Text("Word:"),
+                sg.InputText(key="-WORD-INPUT-", do_not_clear=False),
+                sg.Button(button_text="Test", key="-WORD-BUT-", disabled=True),
+            ],
+            [
+                sg.Text("Automata File", size=(16, 1)),
+                sg.Input(key="-AUTOMATA-FILE-"),
+                sg.FileBrowse(file_types=(("Text Files", "*.txt"),)),
+                sg.Submit(key="-AUTOMATA-SUBMIT-"),
+            ],
+            [
+                sg.Text("Words File", size=(16, 1)),
+                sg.Input(key="-WORD-FILE-"),
+                sg.FileBrowse(file_types=(("Text Files", "*.txt"),)),
+                sg.Submit(key="-WORD-FILE-SUBMIT-", disabled=True),
+            ],
+            [sg.CloseButton("Close")],
+        ]
+        self._window: sg.Window = sg.Window("Pyautomata", layout)
+        self._aut: Optional[pyautomata.Automata] = None
 
-def create_pair_result_window(pairs: List[Tuple[str, str]]) -> None:
-    """
-    Creates a window to display the accepted pairs
-    """
-    pair_string = ""
-    for word1, word2 in pairs:
-        pair_string += f"{word1}, {word2}\n"
-    layout = [
-        [sg.Text("Accepted pairs:")],
-        [sg.Text(pair_string)],
-        [sg.Button("Ok")],
-    ]
-    window = sg.Window("Pyautomata", layout)
-    while True:
-        event, _ = window.read()
-        if event in (sg.WIN_CLOSED, "Ok"):
-            break
-    window.close()
+    def close(self):
+        """
+        Closes the window (and then deletes it for safety)
+        """
+        self.window.close()
+        del self._window
 
+    def read(self):
+        """
+        Reads the Window
+        Just to not need to write instance.window.read()
+        """
+        return self.window.read()
 
-def test_word_pairs(
-    aut: pyautomata.Automata, pairs: List[Tuple[str, str]]
-) -> None:
-    """
-    Tests the pairs, and then creates a window with the valid pairs
-    """
-    result_pairs = []
-    for word1, word2 in pairs:
-        if aut.check_word(word1)[0] and aut.check_word(word2)[0]:
-            result_pairs.append((word1, word2))
-    create_pair_result_window(result_pairs)
+    @property
+    def window(self):
+        """
+        Returns the program window
+        """
+        return self._window
 
+    @property
+    def automata(self):
+        """
+        Returns the created Automata
+        """
+        return self._aut
 
-def read_word_file(file: str) -> List[Tuple[str, str]]:
-    """
-    Uses the parser to read the word file
-    """
-    p = pyautomata.WordFileParser(file_name=file)
-    return p.parse()
+    def create_automata(self, file: str) -> None:
+        """
+        Creates the automata via the Parser
+        Updates the text and buttons on screen
+        """
+        p = pyautomata.AutomataParser(file_name=file)
+        description, function_program = p.parse()
+        self.window["-AUTOMATA-LOADED-"].update("Loaded")
+        self.window["-WORD-BUT-"].update(disabled=False)
+        self.window["-WORD-FILE-SUBMIT-"].update(disabled=False)
+        self._aut = pyautomata.MinimizedAutomata(
+            function_program, **description
+        )
+        self._aut.minimize()
 
+    @staticmethod
+    def create_pair_result_window(pairs: List[Tuple[str, str]]) -> None:
+        """
+        Creates a window to display the accepted pairs
+        """
+        pair_string = ""
+        for word1, word2 in pairs:
+            pair_string += f"{word1}, {word2}\n"
+        layout = [
+            [sg.Text("Accepted pairs:")],
+            [sg.Text(pair_string)],
+            [sg.Button("Ok")],
+        ]
+        window = sg.Window("Pyautomata", layout)
+        while True:
+            event, _ = window.read()
+            if event in (sg.WIN_CLOSED, "Ok"):
+                break
+        window.close()
 
-def create_result_path_string(result_path: Union[str, List[str]]) -> str:
-    """
-    Creates the path string, or, if it's the rejected message
-    Returns the string unchanged
-    """
-    result = ""
-    if isinstance(result_path, str):
-        return result_path
-    for state, element in grouper(result_path, 2):
-        if element:
-            result += f"{state}, {element} ->\n"
+    def test_word_pairs(self, file: str) -> None:
+        """
+        Given the file
+        Creates the pairs, then test them
+        Opens up a new window with the results
+        """
+        wfp = pyautomata.WordFileParser(file_name=file)
+        result_pairs = []
+        for word1, word2 in wfp.parse():
+            if (
+                self.automata.check_word(word1)[0]
+                and self.automata.check_word(word2)[0]
+            ):
+                result_pairs.append((word1, word2))
+        self.create_pair_result_window(result_pairs)
+
+    @staticmethod
+    def create_result_path_string(result_path: Union[str, List[str]]) -> str:
+        """
+        Creates the path string, or, if it's the rejected message
+        Returns the string unchanged
+        """
+        result = ""
+        if isinstance(result_path, str):
+            return result_path
+        for state, element in grouper(result_path, 2):
+            if element:
+                result += f"{state}, {element} ->\n"
+            else:
+                result += str(state)
+        return result
+
+    def make_result_window(
+        self, result: bool, reason_or_path: Union[str, List[str]]
+    ) -> None:
+        """
+        Makes a Window to say if the word
+        Was rejected or accepted
+        """
+        layout = []
+        result_string = self.create_result_path_string(reason_or_path)
+        if result:
+            layout = [
+                [sg.Text("Word accepted.")],
+                [sg.Text(f"Path:\n{result_string}")],
+                [sg.Button("Ok")],
+            ]
         else:
-            result += str(state)
-    return result
+            layout = [
+                [sg.Text("Word rejected.")],
+                [sg.Text(f"Reason:\n{result_string}")],
+                [sg.Button("Ok")],
+            ]
 
+        result_window = sg.Window("Pyautomata", layout)
+        while True:
+            event, _ = result_window.read()
+            if event in (sg.WIN_CLOSED, "Ok"):
+                break
+        result_window.close()
 
-def make_result_window(
-    result: bool, reason_or_path: Union[str, List[str]]
-) -> None:
-    """
-    Makes a Window to say if the word
-    Was rejected or accepted
-    """
-    layout = []
-    result_string = create_result_path_string(reason_or_path)
-    if result:
-        layout = [
-            [sg.Text("Word accepted.")],
-            [sg.Text(f"Path:\n{result_string}")],
-            [sg.Button("Ok")],
-        ]
-    else:
-        layout = [
-            [sg.Text("Word rejected.")],
-            [sg.Text(f"Reason:\n{result_string}")],
-            [sg.Button("Ok")],
-        ]
-
-    result_window = sg.Window("Pyautomata", layout)
-    while True:
-        event, _ = result_window.read()
-        if event in (sg.WIN_CLOSED, "Ok"):
-            break
-    result_window.close()
-
-
-def test_word(window: sg.Window, word: str, aut: pyautomata.Automata) -> None:
-    """
-    Tests a word on the automata
-    And draws a window with the results
-    """
-    window["-WORD-TO-TEST-"].update(word)
-    result, reason_or_path = aut.check_word(word)
-    make_result_window(result, reason_or_path)
-
-
-def setup() -> sg.Window:
-    """
-    Function that creates the layout.
-    Returns an window with said layout.
-    """
-    sg.theme("DarkGrey8")
-    layout = [
-        [
-            sg.Text("Automata: "),
-            sg.Text("Not loaded", key="-AUTOMATA-LOADED-"),
-        ],
-        [
-            sg.Text("Last word:"),
-            sg.Text("No word loaded", key="-WORD-TO-TEST-"),
-        ],
-        [
-            sg.Text("Word:"),
-            sg.InputText(key="-WORD-INPUT-", do_not_clear=False),
-            sg.Button(button_text="Test", key="-WORD-BUT-", disabled=True),
-        ],
-        [
-            sg.Text("Automata File", size=(16, 1)),
-            sg.Input(key="-AUTOMATA-FILE-"),
-            sg.FileBrowse(file_types=(("Text Files", "*.txt"),)),
-            sg.Submit(key="-AUTOMATA-SUBMIT-"),
-        ],
-        [
-            sg.Text("Words File", size=(16, 1)),
-            sg.Input(key="-WORD-FILE-"),
-            sg.FileBrowse(file_types=(("Text Files", "*.txt"),)),
-            sg.Submit(key="-WORD-FILE-SUBMIT-", disabled=True),
-        ],
-        [sg.CloseButton("Close")],
-    ]
-    return sg.Window("Pyautomata", layout)
+    def test_word(self, word: str) -> None:
+        """
+        Tests a word on the automata
+        And draws a window with the results
+        """
+        self.window["-WORD-TO-TEST-"].update(word)
+        result, reason_or_path = self.automata.check_word(word)
+        self.make_result_window(result, reason_or_path)
